ee923493d0c613f37027479c1d75027bae9a5ea8
boshyxd <poisonhick@gmail.com>
2025-01-28 14:27:14 -0500

    updated for netlify

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/angusbailey-portfolio/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/angusbailey-portfolio' : '',
   trailingSlash: true,
 };
 
